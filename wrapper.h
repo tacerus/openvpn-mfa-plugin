023a9cf6d9032f2ae5a02bc257dbd559c7259273
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2026-04-14 14:47:53 +0200

    Adapt wrapper to different header locations
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- wrapper.h
+++ wrapper.h
@@ -1,1 +1,20 @@
-#include <openvpn-plugin.h>
+#if defined(__has_include)
+
+#  if __has_include(<openvpn-plugin.h>)
+     // SUSE
+#    include <openvpn-plugin.h>
+
+#  elif __has_include(<openvpn/openvpn-plugin.h>)
+     // Debian
+#    include <openvpn/openvpn-plugin.h>
+
+#  else
+#    error "Could not find openvpn-plugin.h in any known location."
+
+#  endif
+
+#else
+     // Should not happen
+#    error "Macro __has_include is not defined."
+
+#endif
